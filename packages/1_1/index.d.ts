81ca1d7e86e8a997ccf12b6fa0b9bdae5cb74c32
Teddy Bradford <3684553+teddybradford@users.noreply.github.com>
2022-02-18 23:00:50 -0500

    Add named export for Feed

--- packages/1_1/index.d.ts
+++ packages/1_1/index.d.ts
@@ -1,4 +1,5 @@
 export { default } from './types/feed'
+export { default as Feed } from './types/feed'
 export { default as Attachment } from './types/attachment'
 export { default as Author } from './types/author'
 export { default as Hub } from './types/hub'
