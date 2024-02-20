05cb536263c2cb025e704c00b0e2368090e7b057
Ash <ashley@juice49.com>
2024-02-20 19:11:21 +0000

    Prepare `@json-feed-types/common` for JSR

--- packages/common/index.d.ts
+++ packages/common/index.d.ts
@@ -1,3 +1,3 @@
-export { default as BaseFeed } from './types/base-feed'
-export { default as Extension } from './types/extension'
-export { default as ExtensionMap } from './types/extension-map'
+export { default as BaseFeed } from './types/base-feed.ts'
+export { default as Extension } from './types/extension.ts'
+export { default as ExtensionMap } from './types/extension-map.ts'
--- packages/common/types/extension-map.ts
+++ packages/common/types/extension-map.ts
@@ -1,4 +1,4 @@
-import Extension from './extension'
+import Extension from './extension.ts'
 
 type ExtensionMap = Partial<
   Record<'feed' | 'author' | 'item' | 'attachment' | 'hub', Extension>
